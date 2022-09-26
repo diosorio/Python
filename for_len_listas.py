ec5cb6e85429e48cd35522ad57c71e4d7fa7ce0d
Diego Osorio <74689969+diosorio@users.noreply.github.com>
2022-09-26 18:40:02 -0300

    for_len_listas.py

---
+++ for_len_listas.py
@@ -0,0 +1,6 @@
+#creo una lista de 4 elementos
+a = ['Mary', 'tenia', 'un', 'corderito']
+
+#la sentencia FOR recorre los elementos de la lista
+for i in range(len(a)): #la funcion len obtiene el nro index de cada elemento de la lista y lo asigna a variable a
+    print(i, a[i])
