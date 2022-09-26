b5a2daa79730feab7f129fde73e4deddc3441631
Diego Osorio <74689969+diosorio@users.noreply.github.com>
2022-09-26 18:31:04 -0300

    Update funcion_suma.py

--- funcion_suma.py
+++ funcion_suma.py
@@ -1,9 +1,11 @@
+#creamos la función add y le pasaremos el parámetro "a"
 def add(a):
     """
     add 1 to a
     """
-    b = a + 1
+    b = a + 1 #al parámetro a le sumamos 1 y guardamos su valor en la variable b
     print(a, "if you add one, is:", b)
     return(b)
 
-add(23)
+#invocamos la función, pasandole 23 al parámetro "a"
+add(23) 
